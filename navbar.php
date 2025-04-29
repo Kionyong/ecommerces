6eeba6e0f1fbf0cf0b4ca9533d33bd54a4b05eaf
kionyong <kionyong@2gmail.com>
2025-04-29 15:00:41 +0700

    added file navbar style to in git

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div id="nav-bar">
+        <h1>SHOES SHOP</h1>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Products</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Checkout</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
