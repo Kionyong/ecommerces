df021f6d9b2235a6e8fb56ffd793f489d6a15871
kionyong <kionyong@2gmail.com>
2025-04-30 14:54:26 +0700

    changed branch kion to main

--- home.php
+++ home.php
@@ -4,10 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="./kion.css">
 </head>
 <body>
+    <?php 
+        include("navbar.php");
+    ?>
+    <!--slide-->
     <div id="slide">
-
+        <img src="./assets/logodrink.jpg" alt="">
+    </div>
+    <!--text-->
+    <h1 id="h1">INTRODUCTIONS</h1>
+    <!--body-->
+    <div class="layout">
+        <div class="box">
+            <h2>Coca Cola</h2>
+            <p>
+                Coca-Cola is a carbonated beverage made by the Coca-Cola company based in Atlanta, Georgia.
+                The brand name originates from its two key ingredients: cocaine and caffeine, which are extracted
+                 respectively from the coca leaf and the kola nut, hence its name Coca-Cola.
+            </p>
+            <img src="./assets/cocola.png" alt="">
+        </div>  
+        <div class="box">
+            <h2>Lactasoy</h2>
+            <p>
+            Great selection with our Original Classic taste! The perfect combination
+             of natural protein from soybean and whole milk powder provides the highest nutrition with the finest taste.
+            </p>
+            <img src="./assets/lactasoy.png" alt="">
+        </div>  
+    </div>
+    <!--list of drink-->
+    <div class="list-drink">
+        <img src="./assets/cacocola001.png" alt="">  
+        <img src="./assets/lactasoy_logo002.png" alt="">  
     </div>
 </body>
 </html>
