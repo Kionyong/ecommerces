ea0115902f3dfc910cc46c920517354e130ba163
kionyong <kionyong@2gmail.com>
2025-05-06 14:23:38 +0700

    edit file footer...

--- footer.php
+++ footer.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
-    <link rel="stylesheet" href="./footer.php">
+    <link rel="stylesheet" href="./kion.css">
 </head>
 <body>
     <footer id="footer">
         <div class="">
-            <h1>SHOES SHOP</h1>
+            <h1>Drinks</h1>
         </div>
         <div class=""></div>
     </footer>
